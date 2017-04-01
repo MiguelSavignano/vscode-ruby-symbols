cbb9d131596271a9464602ac38bdd5cead8131c9
Miguel Savignano <migue.masx@gmail.com>
2017-04-01 16:01:28 +0200

    add example

---
+++ test/examples/example2.rb
@@ -0,0 +1,23 @@
+class MyClass
+
+  before_validation(on: :create) do
+    ...
+  end
+
+  after_initialize do
+    ...
+  end
+
+  loop do
+    ...
+  end
+
+  scope :within, ->(time) do
+    where('created_at >= ?', time.ago)
+  end
+
+  def hi
+    12
+  end
+
+end
