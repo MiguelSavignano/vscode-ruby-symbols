840ef3ce4050da98480396e61df79ef6d1bc5ee3
Miguel Savignano <miguel.savignano@the-cocktail.com>
2019-06-23 17:07:33 +0200

    debugger

--- src/extension.ts
+++ src/extension.ts
@@ -4,16 +4,7 @@ import RubyDocumentSymbolProvider from "./ruby_document_symbol_provider";
 
 export function activate(context: ExtensionContext) {
   console.log("ruby symbols active v 0.1.4");
-  var selector = {
-    language: "ruby",
-    scheme: "file"
-  };
-  context.subscriptions.push(
-    languages.registerDocumentSymbolProvider(
-      selector,
-      new RubyDocumentSymbolProvider()
-    )
-  );
+  debugger;
 }
 
 export function deactivate() {}
